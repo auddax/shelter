7f708f73cdff168636afafdf7b163babad885161
Kirill Konovalov <auddax@gmail.com>
2022-04-20 19:18:20 +0300

    fix: set width for mobile menu, add dark overlay while mobile menu is open

--- shelter/assets/src/script.js
+++ shelter/assets/src/script.js
@@ -1,34 +1,25 @@
-let navbar = document.querySelectorAll('.navbar li');
-let burger = document.querySelector('.burger')
-let burgerNav = document.querySelectorAll('.burger-menu ul li')
+let burgerIcon = document.querySelector('.burger')
+let burgerList = document.querySelectorAll('.burger-menu ul li')
+let overlayDark = document.querySelector('.overlay-darken')
 
-function toggler() {
-    let target = document.querySelector('header');
-    if (target.classList.contains('menu-active')) {
-        target.classList.remove('menu-active');
-    } else {
-       target.classList.add('menu-active');
+function toggler(event) {
+    let targetClasses = event.target.classList;
+    if (targetClasses.contains('burger-menu-item') || targetClasses.contains('overlay-darken') || targetClasses.contains('burger')) {
+        let target = document.querySelector('body');
+        if (target.classList.contains('menu-active')) {
+            target.classList.remove('menu-active');
+        } else {
+        target.classList.add('menu-active');
+        }
+        event.stopPropagation();
     }
 }
 
-for (let item of navbar) {
-    item.addEventListener('mouseenter', () => {
-        let target = document.querySelector('.focus');
-        if (target === null) return;
-        if (item.textContent === target.textContent) return;
-        target.classList.remove('focus');
-    })
+burgerIcon.addEventListener('click', toggler);
+overlayDark.addEventListener('click', toggler);
+
+for (let item of burgerList) {
+    item.addEventListener('click', toggler);
 }
 
-for (let item of burgerNav) {
-    item.addEventListener('click', toggler)
-}
 
-burger.addEventListener('click', () => {
-    let target = document.querySelector('header');
-    if (target.classList.contains('menu-active')) {
-        target.classList.remove('menu-active');
-    } else {
-       target.classList.add('menu-active');
-    }
-})
